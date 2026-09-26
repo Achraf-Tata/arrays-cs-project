f542b5c7a88c75783cd74e931ddd48b27c38a9b1
Achraf-Tata <achraf22.tata@gmail.com>
2026-09-26 13:32:29 +0000

    Finished questions 9 and 10 and implemented the main

--- src/main/java/samplearrays/ManageStudent.java
+++ src/main/java/samplearrays/ManageStudent.java
@@ -11,7 +11,7 @@ public class ManageStudent {
             return null;
         }
         Student oldest = students[0];
-        for(Student s: students){if(s.getAge()>=oldest.getAge();){oldest=s;}}
+        for(Student s: students){if(s.getAge()>=oldest.getAge()){oldest=s;}}
         return oldest;
     }
 
@@ -26,7 +26,7 @@ public class ManageStudent {
     public static double averageGrade(Student[] students) {
         if (students.length==0){return Double.NaN;}
         double sum=0;
-        for(Student s: students){sum+=s.getGrade;}
+        for(Student s: students){sum+=s.getGrade();}
         return sum/students.length;
     }
 
@@ -59,17 +59,31 @@ public class ManageStudent {
 
     // 9) Find Duplicate Names
     public static boolean hasDuplicateNames(Student[] students) {
-        
+        for(int i=0;i< students.length;i++){
+            for(int j=i+1;j< students.length;j++){
+                if (students[i].getName().equals(students[j].getName())){
+                    return true;
+                }
+            }
+        }return false;
     }
-
     // 10) Expandable Array: return a new array with one more slot and append student
     public static Student[] appendStudent(Student[] students, Student newStudent) {
-
+        Student[] temp = new Student[students.length+1];
+        for(int i=0;i<students.length;i++) {temp[i]=students[i];}
+        temp[students.length]=newStudent;
+        return temp;
     }
 
     // 1) Create an Array of Students + demos for all tasks
     public static void main(String[] args) {
         // Create & initialize array of 5 students
+        Student[] arr=new Student[5];
+        arr[0]=new Student(1,"Student1",19,19);
+        arr[1]=new Student(2,"Student2",20,18);
+        arr[2]=new Student(3,"Student3",21,20);
+        arr[3]=new Student(4,"Dina",20,19);
+        arr[4]=new Student(5,"Student5",19,18);
 
 
         // Print all
@@ -78,19 +92,23 @@ public class ManageStudent {
         System.out.println("Total created: " + Student.getNumStudent());
 
         // 2) Oldest
-
+        System.out.println("Oldest : " + ManageStudent.findOldest(arr));
 
         // 3) Count adults
-
-
+        System.out.println("Adults count : " + ManageStudent.countAdults(arr));
         // 4) Average grade
-
+        System.out.println("Average grade : " + ManageStudent.averageGrade(arr));
 
         // 5) Find by name
-
+        if(ManageStudent.findStudentByName(arr,"Student3")!=null){
+            System.out.println("Student3 found");
+        }else{
+            System.out.println("Student3 not found");
+        }
 
         // 6) Sort by grade desc
         // sort function
+        ManageStudent.sortByGradeDesc(arr);
         System.out.println("\n== Sorted by grade (desc) ==");
         for (Student s : arr) System.out.println(s);
 
@@ -100,14 +118,41 @@ public class ManageStudent {
 
         // 8) Update grade by id
         // function
+        boolean updated = ManageStudent.updateGrade(arr,4,18);
         System.out.println("\nUpdated id=4? " + updated);
         System.out.println(findStudentByName(arr, "Dina"));
 
         // 9) Duplicate names
-
-
+        if(ManageStudent.hasDuplicateNames(arr)){
+            System.out.println("Duplicate found");
+        }else {
+            System.out.println("No duplicate found");
+        }
         // 10) Append new student
+        arr=ManageStudent.appendStudent(arr,new Student(6,"Student6",20,18));
 
+        //11 The class question
+        Student[][] classrooms= new Student[2][3];
+
+        classrooms[0][0]=arr[0];
+        classrooms[0][1]=arr[1];
+        classrooms[0][2]=arr[2];
+        classrooms[1][0]=arr[3];
+        classrooms[1][1]=arr[4];
+        classrooms[1][2]=arr[5];
+
+        for(int i=0;i<classrooms.length;i++){
+            System.out.println("All student in class : "+i);
+            for (int j=0;j<classrooms[i].length;j++){
+                System.out.println(classrooms[i][j]);
+            }
+        }
+
+        for(int i=0;i<classrooms.length;i++){
+            System.out.println("Best student in class : "+i);
+            ManageStudent.sortByGradeDesc(classrooms[i]);
+            System.out.println(classrooms[i][0]);
+        }
     }
 }
 
